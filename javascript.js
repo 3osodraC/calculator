82208dd3483ebc578dc0a54d72f7b0fb6f2efaf8
Vitor Cardoso <vitorcnf64@gmail.com>
2022-04-11 21:29:28 -0300

    Fixed calculation, removed '.' & 'del', style changes, comments. more on desc.
    Change numArray population method, revamped calculation, equals and clear button are now functional, now the small display shows the operation, and the big one the result and typing, removed unnecessary variables and added more detailed comments.

--- javascript.js
+++ javascript.js
@@ -1,42 +1,80 @@
-const display = document.querySelector('.operation-display')
+const bigDisplay = document.querySelector('.big-display')
+    , smallDisplay = document.querySelector('.small-display')
 
     , clearBtn = document.querySelector('.clear-btn')
-    , delBtn = document.querySelector('.del-btn')
-    , pointBtn = document.querySelector('.point-btn')
-    , equalsBtn = document.querySelector('.equals-btn')
-
     , numberButtons = document.querySelectorAll('.number')
     , operatorButtons = document.querySelectorAll('.operator')
     , buttons = document.querySelectorAll('button');
 
-
 let operator = ''
-  , result = ''
+  , prevOp = '' // Previous operator. A work-around for erroneous calculations.
+  , result
+  , newDisplay = false
   , numArray = [];
 
+// Resets bigDisplay when a new number is clicked after an operator. i.e.:
+// Small:    | --------> | 63+ | --------> | 63+ |
+// Big  : 63 | + clicked | 63  | 3 clicked | 3   |
+numberButtons.forEach(button => button.addEventListener('click', (e) => {
+    if(newDisplay) {
+        bigDisplay.textContent = '';
+        newDisplay = false;
+    }
+    prevOp = operator;
+}));
+
+// Populates bigDisplay and (1) prevents operator spam.
 buttons.forEach(button => button.addEventListener('click', (e) => {
 	if(e.target.textContent.match(/[0-9]/)) {
-		display.textContent = `${display.textContent + e.target.textContent}`;
+		bigDisplay.textContent = `${bigDisplay.textContent + e.target.textContent}`;
 	}
 
-	// Disables operator spam, i.e. ++++----xxx-+.
-	if(e.target.textContent.match(/[+×÷-]/) && display.textContent != '' &&
-	!display.textContent[display.textContent.length - 1].match(/[+×÷-]/)) {
-		display.textContent = `${display.textContent + e.target.textContent}`;
-		operator = e.target.textContent;
+	// (1) Populates operator & bigDisplay with spam prevention i.e.: '+++-xxx+'.
+	if(e.target.textContent.match(/[+×÷-]/) && bigDisplay.textContent != '' &&
+	!bigDisplay.textContent[bigDisplay.textContent.length - 1].match(/[+×÷-]/)) {
+		bigDisplay.textContent = `${bigDisplay.textContent + e.target.textContent}`;
 	}
 }));
 
+// Populates numArray, smallDisplay & operator. Also calculates.
 operatorButtons.forEach(button => button.addEventListener('click', (e) => {
-    numArray = display.textContent.split(/[-+×÷]/);
-	numArray = numArray.slice(0, -1); // Removes empty item in the end of array.
+    operator = e.target.textContent;
+    if(bigDisplay.textContent.match(/[0-9]/)) {
+        numArray.push(parseInt(bigDisplay.textContent));
+    }
+    console.log(numArray);
 
+    smallDisplay.textContent = bigDisplay.textContent; // This might mess up the display.
+    if(numArray[numArray.length - 1] !== undefined) {
+        bigDisplay.textContent = `${numArray[numArray.length - 1]}`;
+    }
+    newDisplay = true;
+
+    // Calculation
 	if(numArray.length > 1) {
-		
+        const index = numArray.length - 1;
+		result = operate(numArray[index - 1], prevOp, numArray[index]);
+        numArray.push(result);
+
+        if(e.target.textContent === '=') {
+            smallDisplay.textContent = `${numArray[index - 1] + prevOp + numArray[index]}=`;
+        } else {
+            smallDisplay.textContent = `${result + operator}`;
+        }
+        bigDisplay.textContent = `${result}`;
 	}
 }));
 
+clearBtn.addEventListener('click', () => {
+    operator = '';
+    prevOp = '';
+    result = 0;
+    newDisplay = false;
+    numArray = [];
 
+    smallDisplay.textContent = '';
+    bigDisplay.textContent = '';
+});
 
 // Functions
 function operate(x, op, y) {
